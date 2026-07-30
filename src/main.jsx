e3b839b47e65857642ed7d7be9408b6a212fd843
Primal Code <primalrsservice@gmail.com>
2026-07-30 17:10:43 +0200

    Wrap application with EventProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { EventPage } from './pages/EventPage';
-import { EventsPage } from './pages/EventsPage';
-import { Provider } from './components/ui/provider';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Root } from './components/Root';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EventPage } from "./pages/EventPage";
+import { EventsPage } from "./pages/EventsPage";
+import { Provider } from "./components/ui/provider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./components/Root";
+import { EventProvider } from "./context/EventContext";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        children: [
-            {
-                path: '/',
-                element: <EventsPage />,
-                // loader: postListLoader,
-            },
-            {
-                path: '/event/:eventId',
-                element: <EventPage />,
-                // loader: postLoader,
-                // action: addComment,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <EventsPage />,
+        // loader: postListLoader,
+      },
+      {
+        path: "/event/:eventId",
+        element: <EventPage />,
+        // loader: postLoader,
+        // action: addComment,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider>
-            <RouterProvider router={router} />
-        </Provider>
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EventProvider>
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
+    </EventProvider>
+  </React.StrictMode>,
 );
