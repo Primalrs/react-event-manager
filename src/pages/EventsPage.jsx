b8dde9bad1a7652b2bfa946b89c80bd8440a7923
Primal Code <primalrsservice@gmail.com>
2026-07-30 17:39:26 +0200

    Display events from EventContext

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,7 +1,25 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading } from "@chakra-ui/react";
+import { useContext } from "react";
+import { EventContext } from "../context/EventContext";
 
 export const EventsPage = () => {
-    return (
-        <Heading>List of events</Heading>
-    );
+  const { events, loading, error } = useContext(EventContext);
+
+  if (loading) {
+    return <Heading>Loading...</Heading>;
+  }
+
+  if (error) {
+    return <Heading>Error: {error}</Heading>;
+  }
+
+  return (
+    <>
+      <Heading mb={4}>List of events</Heading>
+
+      {events.map((event) => (
+        <p key={event.id}>{event.title}</p>
+      ))}
+    </>
+  );
 };
